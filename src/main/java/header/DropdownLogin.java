9e2642bd1521c836a03af496503247ec231e5f92
cath3r0 <ilghvm@gmail.com>
2016-06-07 12:55:14 +0300

    [changed] dropdownLogin, dropdownRegister, header moved to new package

---
+++ src/main/java/header/DropdownLogin.java
@@ -0,0 +1,36 @@
+package header;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class DropdownLogin {
+	@FindBy(xpath = "/html/body/header/div/div/div[2]/nav/div/div[2]/ul/li[7]/a")
+	public WebElement dropdownLoginButton;
+
+	@FindBy(xpath = "/html/body/header/div/div/div[2]/nav/div/div[2]/ul/li[7]/ul/div/form/div[1]/input")
+	public WebElement dropdownEmailField;
+
+	@FindBy(xpath = "/html/body/header/div/div/div[2]/nav/div/div[2]/ul/li[7]/ul/div/form/div[2]/input")
+	public WebElement dropdownPasswordField;
+
+	@FindBy(xpath = "/html/body/header/div/div/div[2]/nav/div/div[2]/ul/li[7]/ul/div/form/div[3]/div/div[1]/input")
+	public WebElement dropdownRememberMeCheckbox;
+
+	@FindBy(xpath = "/html/body/header/div/div/div[2]/nav/div/div[2]/ul/li[7]/ul/div/form/div[3]/div/div[2]/input")
+	public WebElement dropdownLoginSubmitButton;
+
+	@FindBy(xpath = "/html/body/header/div/div/div[2]/nav/div/div[2]/ul/li[6]/a")
+	public WebElement dropdownLogoutButton;
+
+	public void loggingIn(String email, String password) {
+		dropdownLoginButton.click();
+		dropdownEmailField.sendKeys(email);
+		dropdownPasswordField.sendKeys(password);
+		dropdownLoginSubmitButton.click();
+	}
+
+	public void logout() {
+		dropdownLogoutButton.click();
+	}
+}
+
---
+++ src/main/java/header/Header.java
@@ -0,0 +1,27 @@
+package header;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Header {
+
+    public static final String adminButtonXPATH = "/html/body/header/div/div/div[2]/nav/div/div[2]/ul/li[6]/a";
+    public static final String adminButtonText = "Admin";
+    public static final String patientsButtonXPATH = "/html/body/header/div/div/div[2]/nav/div/div[2]/ul/li[6]/a";
+    public static final String patientsButtonText = "Patients";
+
+    //only for admin
+    @FindBy(linkText = adminButtonText)
+    public static WebElement adminButtonByText;
+
+    @FindBy(xpath = adminButtonXPATH)
+    public WebElement adminButtonByXpath;
+
+    //only for doctors
+    @FindBy(xpath = patientsButtonXPATH)
+    public static WebElement patientsButtonByXpath;
+
+    @FindBy(linkText = patientsButtonText)
+    public static WebElement patientsButtonByText;
+
+}
