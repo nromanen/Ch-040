3bdbf9c2457dd80c91bd11941d67f5758041e316
cath3r0 <ilghvm@gmail.com>
2016-06-07 12:40:42 +0300

    [fixed] baseTest urls

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -8,15 +8,15 @@ public class BaseTest {
     public BrowserWrapper browserAction;
     public static final String HOME_URL = "http://91.209.24.68/ch-040/";
     public static final String DOCTOR_PAGE_URL = HOME_URL.concat("hospital/1/department/1");
-    public static final String ADMIN_DASHBOARD_URL = HOME_URL.concat("/admin/dashboard");
-    public static final String ADDING_NEW_HOSPITAL_URL = HOME_URL.concat("/admin/map/new");
-    public static final String HOSPITALS_URL = HOME_URL.concat("/hospitals");
-    public static final String DOCTOR_URL = HOME_URL.concat("/dashboard?id=37&did=1");
-    public static final String PATIENTS_LIST_URL = HOME_URL.concat("/patients");
-    public static final String LOGIN_URL = HOME_URL.concat("/login");
-    public static final String REGISTER_URL = HOME_URL.concat("/newUser");
-    public static final String FIND_URL = HOME_URL.concat("/mapsearch");
-    public static final String VALIDATE_URL = HOME_URL.concat("/admin/map/validate");
+    public static final String ADMIN_DASHBOARD_URL = HOME_URL.concat("admin/dashboard");
+    public static final String ADDING_NEW_HOSPITAL_URL = HOME_URL.concat("admin/map/new");
+    public static final String HOSPITALS_URL = HOME_URL.concat("hospitals");
+    public static final String DOCTOR_URL = HOME_URL.concat("dashboard?id=37&did=1");
+    public static final String PATIENTS_LIST_URL = HOME_URL.concat("patients");
+    public static final String LOGIN_URL = HOME_URL.concat("login");
+    public static final String REGISTER_URL = HOME_URL.concat("newUser");
+    public static final String FIND_URL = HOME_URL.concat("mapsearch");
+    public static final String VALIDATE_URL = HOME_URL.concat("admin/map/validate");
 
 
 
