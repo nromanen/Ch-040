8a129eb46c7f27efda0c1393b44bee40fc855726
cath3r0 <ilghvm@gmail.com>
2016-06-09 17:41:03 +0300

    [+] new admin login and password

--- src/test/java/hospitalSeeker/BaseTest.java
+++ src/test/java/hospitalSeeker/BaseTest.java
@@ -20,8 +20,8 @@ public class BaseTest {
     public static final String FIND_URL = HOME_URL.concat("mapsearch");
     public static final String VALIDATE_URL = HOME_URL.concat("admin/map/validate");
 
-    public static final String ADMIN_LOGIN = "admin@ad.ad";
-    public static final String ADMIN_PASSWORD = "qwer";
+    public static final String ADMIN_LOGIN = "admin@hospitals.ua";
+    public static final String ADMIN_PASSWORD = "1111";
     public static final String MANAGER_LOGIN = "manager@com.com";
     public static final String MANAGER_PASSWORD = "manager";
     public static final String DOCTOR_LOGIN = "doctor@com.com";
