7cb9d9c20a2e356d6ef3797abc3ef6e721b03d7a
cath3r0 <ilghvm@gmail.com>
2016-06-09 10:28:48 +0300

    [+] tests in TestDoctorRole

--- src/test/java/hospitalSeeker/roles/TestDoctorRole.java
+++ src/test/java/hospitalSeeker/roles/TestDoctorRole.java
@@ -1,6 +1,55 @@
 package hospitalSeeker.roles;
 
-import hospitalSeeker.BaseTest;
+import hospitalSeeker.header.Header;
+import org.testng.annotations.*;
 
-public class TestDoctorRole extends BaseTest {
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+public class TestDoctorRole extends BaseRoleTest {
+
+    // TODO: 09.06.16 what if use browser instead of browserAction?
+    @BeforeClass
+    public void beforeClass() {
+        super.beforeClass();
+    }
+
+    @BeforeMethod
+    public void beforeMethod(){
+        browserAction.goTo(HOME_URL);
+        dropdownLogin.loggingIn(DOCTOR_LOGIN, DOCTOR_PASSWORD);
+    }
+
+    @Test
+    public void testAccessDeniedToAdminDashboardForDoctors() {
+        assertFalse(browserAction.isElementPresent(Header.adminButtonByText), "element is present!");
+        assertFalse(browserAction.isElementPresent(adminPage.dashboardTable), "element is present!");
+        browserAction.goTo(ADMIN_DASHBOARD_URL);
+        assertTrue(browserAction.containsText("not authorized to access"), "access not denied");
+    }
+
+    @Test
+    public void testAccessDeniedToAddingNewHospitalForDoctors() {
+        assertFalse(browserAction.isElementPresent(adminPage.optionsButton), "element is present");
+        browserAction.goTo(ADDING_NEW_HOSPITAL_URL);
+        assertTrue(browserAction.containsText("not authorized to access"), "access not denied");
+    }
+
+    @Test
+    public void testAccessDeniedToPatientsListForDoctor() {
+        assertTrue(browserAction.isElementPresent(Header.patientsButtonByXpath), "element is present");
+        browserAction.goTo(PATIENTS_LIST_URL);
+        assertTrue(browserAction.containsText("Patient"), "access denied");
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        super.afterMethod();
+    }
+
+    @AfterClass
+    public void afterClass() {
+        super.afterClass();
+    }
+
 }
