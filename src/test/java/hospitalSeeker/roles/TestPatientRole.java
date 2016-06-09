63d59f95df0cc8dd7bc96878d42d8d811c2bfef9
cath3r0 <ilghvm@gmail.com>
2016-06-09 10:33:23 +0300

    [+] tests in TestPatientRole

--- src/test/java/hospitalSeeker/roles/TestPatientRole.java
+++ src/test/java/hospitalSeeker/roles/TestPatientRole.java
@@ -1,6 +1,54 @@
 package hospitalSeeker.roles;
 
-import hospitalSeeker.BaseTest;
+import hospitalSeeker.header.Header;
+import org.testng.annotations.*;
 
-public class TestPatientRole extends BaseTest {
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+public class TestPatientRole extends BaseRoleTest {
+
+    @BeforeClass
+    public void beforeClass() {
+        super.beforeClass();
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        browserAction.goTo(HOME_URL);
+        dropdownLogin.loggingIn(PATIENT_LOGIN, PATIENT_PASSWORD);
+    }
+
+    @Test
+    public void testAccessDeniedToAdminDashboardForPatients() {
+        assertFalse(browserAction.isElementPresent(Header.adminButtonByText), "element is present");
+        assertFalse(browserAction.isElementPresent(adminPage.dashboardTable), "element is present");
+        browserAction.goTo(ADMIN_DASHBOARD_URL);
+        assertTrue(browserAction.containsText("not authorized to access"), "access not denied");
+    }
+
+    @Test
+    public void testAccessDeniedToAddingNewHospitalForPatients() {
+        assertFalse(browserAction.isElementPresent(adminPage.optionsButton), "element is present");
+        browserAction.goTo(ADDING_NEW_HOSPITAL_URL);
+        assertTrue(browserAction.containsText("not authorized to access"), "access not denied");
+    }
+
+    @Test
+    public void testAccessDeniedToPatientsListForPatient() {
+        assertFalse(browserAction.isElementPresent(Header.patientsButtonByText), "element is present");
+        browserAction.goTo(PATIENTS_LIST_URL);
+        assertTrue(browserAction.containsText("Log in"), "access not denied");
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        super.afterMethod();
+    }
+
+    @AfterClass
+    public void afterClass() {
+        super.afterClass();
+    }
+
 }
