cfb8f8da63b44f32a3d91420842a451665201c46
Надежда <nadjacv001@gmail.com>
2016-06-21 16:26:24 +0300

    remaked tests according to a new version

--- src/main/java/hospitalSeeker/RegisterPage.java
+++ src/main/java/hospitalSeeker/RegisterPage.java
@@ -9,34 +9,21 @@ public class RegisterPage {
 
 	public static final String SUCCESSFULL_REGISTRATION = "/html/body/section/div/div/div/h3";
 	public static final String THIS_USER_IS_ALREADY_EXIST = "/html/body/section/div/div/div/h3";
-	public static final String REGISTER_FORM = "/html/body/section/div/div/div";
 
 	@FindBy(id = "email")
 	public WebElement emailRegister;
 
 	@FindBy(id = "password")
 	public WebElement passwordRegister;
-	
-	@FindBy(id = "enabled")
-	public WebElement enabledButton;
-	
-	@FindBy(css = "option[value=ADMIN]")
-	public WebElement roleAdmin;
-	
-	@FindBy(css = "option[value=DOCTOR]")
-	public WebElement roleDoctor;
-	
-	@FindBy(css = "option[value=MANAGER]")
-	public WebElement roleManager;
-	
-	@FindBy(css = "option[value=PATIENT]")
-	public WebElement rolePatient;
-	
-	@FindBy(css = "input.btn.btn-primary.btn-sm")
+
+	@FindBy(id = "confirmPassword")
+	public WebElement confirmPasswordRegister;
+
+	@FindBy(id = "registerSubmit")
 	public WebElement registerButton;
 	
-	@FindBy(xpath = "/html/body/section/div/div/div/div/form/div[5]/div/a")
-	public WebElement cancelButton;
+	@FindBy(id = "loginRedirect")
+	public WebElement loginButton;
 
 	public static RegisterPage init(WebDriver driver) {
 		return PageFactory.initElements(driver, RegisterPage.class);
