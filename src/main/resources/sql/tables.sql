103215b75a8104617b9e4205949c138dcff52001
cvkual <cvkual@gmail.com>
2016-06-21 17:44:46 +0300

    [fixed] foreign keys, set security

--- src/main/resources/sql/tables.sql
+++ src/main/resources/sql/tables.sql
@@ -7,7 +7,7 @@ SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
-SET row_security = off;
+--SET row_security = off;
 
 
 CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
