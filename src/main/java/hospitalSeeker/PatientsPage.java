141a47ea90df2b111078bb703f1c71d02ceb18f0
Korop Mykola <koropmykola@gmail.com>
2016-07-13 15:03:03 +0300

    New locators to PatientPage

--- src/main/java/hospitalSeeker/PatientsPage.java
+++ src/main/java/hospitalSeeker/PatientsPage.java
@@ -7,36 +7,51 @@ import org.openqa.selenium.support.PageFactory;
 
 public class PatientsPage {
 
-
-	@FindBy (linkText="patient2@i.ua")
-	public WebElement patient2iua;
+	@FindBy (linkText="patient.sf@hospitals.ua")
+	public WebElement patientsf;
 	
-	@FindBy (linkText="tutu@ukr.net")
-	public WebElement tutu;
+	@FindBy (linkText="patient.rr@hospitals.ua")
+	public WebElement patientrr;
 	
-	@FindBy (linkText="doctor@gmail.Com")
-	public WebElement doctorgmailCom;
+	@FindBy (linkText="patient.ml@hospitals.ua")
+	public WebElement patientml;
 	
-	@FindBy (linkText="nadja_cv@mail.ru")
-	public WebElement nadja_cvmailru;
+	@FindBy (linkText="patient.in@hospitals.ua")
+	public WebElement patientin;
 	
-	@FindBy (linkText="iamapatient@pat.com")
-	public WebElement iamapatientpatcom;
+	@FindBy (linkText="patient.cd@hospitals.ua")
+	public WebElement patientcd;
 	
-	@FindBy (linkText="olegolegovich1996@gmail.com")
-	public WebElement olegolegovich1996gmailcom;
+	@FindBy (xpath = "//table/thead/tr/th[2]/div[2]/a/i")
+	public WebElement sortButtonByEmail;
 	
-	@FindBy (linkText="archer@mail.ru")
-	public WebElement archermailru;
+	@FindBy (xpath = "//table/thead/tr/th[3]/div[2]/a/i")
+	public WebElement sortButtonByFirstName;
 	
-	@FindBy (linkText="nadja_cv8@mail.ru")
-	public WebElement nadja_cv8mailru;
+	@FindBy (xpath = "//table/thead/tr/th[4]/div[2]/a/i")
+	public WebElement sortButtonByLastName;
 	
-	@FindBy (linkText="nadjacv51@mail.ru")
-	public WebElement nadjacv51mailru;
+	@FindBy (id = "usr")
+	public WebElement searchField;
+	
+	@FindBy (css = "button.btn.btn-default")
+	public WebElement submitButton;
+	
+	@FindBy (css = "a.back-to-top")
+	public WebElement backToTopButton;
+	
+	@FindBy (xpath = "html/body/section/div/div/div[2]/div/div/ul/li[3]/a")
+	public WebElement secondPage;
+	
+	@FindBy (xpath = ".//*[@id='bs-example-navbar-collapse-1']/ul/li[6]/a")
+	public WebElement doctorLink;
+	
+	@FindBy (xpath = ".//*[@id='dropdawn']/li[2]/a")
+	public WebElement logoutButton;
 
 	public static PatientsPage init(WebDriver driver) {
 		return PageFactory.initElements(driver, PatientsPage.class);
 	}
 	
 }
+
